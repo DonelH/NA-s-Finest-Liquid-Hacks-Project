3a278971343a4beb69a6f3c9177fdc7b831c2c36
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-11-07 15:13:38 -0800

    Update NAF_CONTROLLER.py
    Implemented basic camera using opencv-python library

--- NAF_CONTROLLER.py
+++ NAF_CONTROLLER.py
@@ -1,1 +1,20 @@
 # Controller -- Mediator between View & Model including flask
+
+import numpy as np
+import cv2
+
+## Capturing the Video
+
+capture = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = capture.read() # Capture frames
+    
+    cv2.imshow('Camera View', frame) # Display the given frame
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release capture
+capture.release()
+cv2.destroyAllWindows()
+
