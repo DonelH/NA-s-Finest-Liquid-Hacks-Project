8765faf9a01ad3b866e62b4053b45b4dc4c158a0
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-11-08 11:10:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,74 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
+import os
+from dotenv import load_dotenv
 import boto3 
 from boto3.dynamodb.conditions import Key, Attr
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant, IpMessagingGrant
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioException, TwilioRestException
 
-app = Flask(__name__)
-
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
 
 dynamodb = boto3.resource('dynamodb',aws_access_key_id='AKIA5Z3XPAZ2CNF5IF5Q', 
                            aws_secret_access_key='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', 
                            region_name='us-east-1')
 
+try:
+    twilio_client = Client()
+except TwilioException:
+    twilio_client = None
+
+app = Flask(__name__)
+
+def get_chatroom(name):
+    if twilio_client is None:
+        return
+    for conversation in twilio_client.conversations.conversations.list():
+        if conversation.friendly_name == name:
+            return conversation
+
+    # a conversation with the given name does not exist ==> create a new one
+    return twilio_client.conversations.conversations.create(
+        friendly_name=name)
+        
 @app.route('/')
 def splash():
     return render_template('splash.html')
 
-@app.route('/login')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('login.html')
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+
+    conversation = get_chatroom(os.environ.get('CHATROOM', 'My Room'))
+    conversation_sid = ''
+    if conversation:
+        try:
+            conversation.participants.create(identity=username)
+        except TwilioRestException as ex:
+            # do not error if the user is already in the conversation
+            if ex.status != 409:
+                raise
+        conversation_sid = conversation.sid
+
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid,
+                        twilio_api_key_secret, identity=username)
+    token.add_grant(VideoGrant(room='My Room'))
+    if conversation:
+        token.add_grant(IpMessagingGrant(
+            service_sid=conversation.chat_service_sid))
+
+    return {'token': token.to_jwt().decode(),
+            'conversation_sid': conversation_sid}
 
 @app.route('/register')
 def register():
